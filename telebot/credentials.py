a52caf9b449a70ad840afe8a8285dac21d294499
Furfin <furf1n228@gmail.com>
2021-09-11 19:26:08 +0300

    third commit

--- telebot/credentials.py
+++ telebot/credentials.py
@@ -1,3 +1,3 @@
 bot_token = "1993698998:AAGkN59E9Er98-WE7mnZwsGDz7G1AcQrqpw"
 bot_user_name = "@FurfinsMtuciSchedule_bot"
-URL = "https://fms-bot.herokuapp.com"
+URL = "https://fms-bot.herokuapp.com/"
